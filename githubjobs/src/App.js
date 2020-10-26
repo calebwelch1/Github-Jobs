492dd907221bd6db4df2f3b19061a7a8080e4ef4
calebwelch <caleb.welch11@gmail.com>
2020-10-26 12:34:24 -0400

    main app skeleton

---
+++ githubjobs/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from "./pages/Landing"
+import Main from "./pages/Main"
+function App() {
+  return (
+    <div className="App">
+    <Main/>
+    </div>
+  );
+}
+
+export default App;
